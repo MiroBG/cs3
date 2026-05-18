72609c2835974e0bf207512b1bae586522cb6e90
MiroBG <557082@student.fontys.nl>
2026-05-18 14:55:10 +0200

    Fix Helm provider configuration
    - Replace exec auth with data source (aws_eks_cluster_auth)
    - Use token-based authentication for Helm provider
    - Resolves IDE linting error: Unexpected block type 'kubernetes'
    - Terraform validate now passes with no errors

--- cs3/terraform/main.tf
+++ cs3/terraform/main.tf
@@ -32,16 +32,15 @@ provider "kubernetes" {
   }
 }
 
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
-    }
+    token                  = data.aws_eks_cluster_auth.this.token
   }
 }
 
